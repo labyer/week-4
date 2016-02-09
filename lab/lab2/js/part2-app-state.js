72ca6b1b9ef8790c6d706b0fad7dca3c68a174f4
labyer <labyer@design.upenn.edu>
2016-02-09 11:40:30 -0500

    Complete Lab 2-2

--- lab/lab2/js/part2-app-state.js
+++ lab/lab2/js/part2-app-state.js
@@ -1,72 +1,101 @@
 /* =====================
   Lab 2, part 2: application state
 
-  Spatial applications aren't typically as simple as putting data on a map. In
-  addition, you'll usually need to change the stored data in response to user
-  input. This lab walks you through writing a set of functions that are capable
-  of building an interactive application.
+  Spatial applications aren't typically as simple as putting data on a map. In addition, you'll usually need to change the stored data in response to user input. This lab walks you through writing a set of functions that are capable of building an interactive application.
 
-  First, we'll need to write a function for loading points onto the map. Choose
-  any dataset from part1 and write a function here to download it, parse it,
-  make it into markers, and plot it. You'll know you've succeeded when you can
-  see markers on the map.
+  First, we'll need to write a function for loading points onto the map. Choose any dataset from part1 and write a function here to download it, parse it, make it into markers, and plot it. You'll know you've succeeded when you can see markers on the map.
 
   NOTE 1: When we have added markers to the map in the past, we have used a line like:
 
        L.marker([50.5, 30.5]).addTo(map);
 
-       This is accomplishing two goals. L.marker([50.5, 30.5]) makes a marker
-       and .addTo(map) adds that marker to the map. In this task, you will be
-       asked to create separate functions: one to create markers and one to
-       add them to the map.
+       This is accomplishing two goals. L.marker([50.5, 30.5]) makes a marker and .addTo(map) adds that marker to the map. In this task, you will beasked to create separate functions: one to create markers and one to  add them to the map.
 
-  NOTE 2: These functions are being called for you. Look to the bottom of this file
-       to see where and how the functions you are defining will be used. Remember
-       that function calls (e.g. func();) which are equal to a value (i.e. you
-       can set a var to it: var result = func();) must use the 'return' keyword.
+  NOTE 2: These functions are being called for you. Look to the bottom of this file to see where and how the functions you are defining will be used. Remember that function calls (e.g. func();) which are equal to a value (i.e. you  can set a var to it: var result = func();) must use the 'return' keyword.
+
        var justOne = function() {
          return 1;
        }
        var one = justOne();
 ===================== */
 
-var downloadData = $.ajax("");
-var parseData = function() {};
-var makeMarkers = function() {};
-var plotMarkers = function() {};
+
+/*
+var data = $.ajax("https://raw.githubusercontent.com/CPLN690-MUSA610/datasets/master/json/philadelphia-bike-crashes-snippet.json").done(function(data){
+  console.log(JSON.parse(data));
+  _.each(JSON.parse(data), function(i){
+    //console.log(i);
+    L.marker([i.LAT, i.LNG]).addTo(map);
+  });
+});
+*/
+
+
+var downloadData = $.ajax("https://raw.githubusercontent.com/CPLN690-MUSA610/datasets/master/json/philadelphia-bike-crashes-snippet.json");
+//console.log(downloadData);
+
+var parseData = function(data) {
+  return JSON.parse(data);
+};
+
+var makeMarkers = function(data) {
+  return _.map(data, function(i){
+    return L.marker([i.LAT, i.LNG]);
+  });
+};
+
+var plotMarkers = function(data) {
+  return _.each(data, function(i){
+    i.addTo(map);
+  });
+};
 
 
 /* =====================
-  Define the function removeData so that it clears the markers you've written
-  from the map. You'll know you've succeeded when the markers that were
-  previously displayed are immediately removed from the map.
+  Define the function removeData so that it clears the markers you've written from the map. You'll know you've succeeded when the markers that were previously displayed are immediately removed from the map.
 
   In Leaflet, the syntax for removing one specific marker looks like this:
 
   map.removeLayer(marker);
 
-  In real applications, this will typically happen in response to changes to the
-  user's input.
+  In real applications, this will typically happen in response to changes to the user's input.
 ===================== */
 
-var removeMarkers = function() {};
+var removeMarkers = function(data) {
+  return _.each(data, function(i){
+    map.removeLayer(i);
+  });
+};
 
 /* =====================
   Optional, stretch goal
-  Write the necessary code (however you can) to plot a filtered down version of
-  the downloaded and parsed data.
+
+  Write the necessary code (however you can) to plot a filtered down version of the downloaded and parsed data.
 
   Note: You can add or remove from the code at the bottom of this file.
 ===================== */
+
+var filterData = function(data){
+  return _.filter(data, function(i){
+    return i.DATE_OF_MO < 5;
+  });
+};
+
+
+
+
 /* =====================
  CODE EXECUTED DOWN HERE!
 ===================== */
 
 downloadData.done(function(data) {
   var parsed = parseData(data);
-  var markers = makeMarkers(parsed);
+  //console.log(parsed);
+  var filtered = filterData(parsed);
+  var markers = makeMarkers(filtered);
+  //console.log(markers);
   plotMarkers(markers);
-  removeMarkers(markers);
+  //removeMarkers(markers);
 });
 
 /* =====================
