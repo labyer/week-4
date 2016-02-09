b2b10e011d57cb7cef73f28ce25428d77333eb55
labyer <labyer@design.upenn.edu>
2016-02-09 10:42:44 -0500

    Complete Lab 2-1

--- lab/lab2/js/part1-ajax-calls.js
+++ lab/lab2/js/part1-ajax-calls.js
@@ -5,31 +5,34 @@
   Treated as a values, functions can be thought of as recipes that other
   functions can use.
 
-  The first 'recipe' you should write is one that returns true for strings with
-  greater than or equal to 5 characters and false for those shorter than 5 characters.
+  The first 'recipe' you should write is one that returns true for strings with greater than or equal to 5 characters and false for those shorter than 5 characters.
 
   This recipe, given to underscore's _.filter will return only the elements we want.
 ===================== */
-var isLengthOfFiveOrMore = function(str) {};
+var isLengthOfFiveOrMore = function(str) {
+  return str.length > 5;
+};
 
 console.log("isLengthOfFiveOrMore success:",
   _.isEqual(_.filter(['this', 'is','a', 'test', 'testing'], isLengthOfFiveOrMore), ['testing']));
 
 
 /* =====================
-  Now write a 'recipe' to console.log the double of a number (i.e. n * 2). Use the
-  function you write along with underscore's _.each to log the double of every
-  number in the provided array.
+  Now write a 'recipe' to console.log the double of a number (i.e. n * 2). Use the function you write along with underscore's _.each to log the double of every number in the provided array.
 ===================== */
-var logDouble = function(num) {};
+var logDouble = function(num) {
+  console.log(num * 2);
+};
 var theArray = [1, 5, 20, 100];
 
+_.each(theArray, function(num){
+  logDouble(num);
+});
 
 /* =====================
-  Given this already defined function, define fizzbuzzArray so that, when mapped
-  over, it will equal ['fizz', 'buzz', 'fizzbuzz'];
+  Given this already defined function, define fizzbuzzArray so that, when mapped over, it will equal ['fizz', 'buzz', 'fizzbuzz'];
 ===================== */
-var fizzbuzzArray = [];
+var fizzbuzzArray = [3, 20, 15];
 var fizzbuzzFunc = function(num) {
   var str = '';
   if (num % 3 === 0) { str = 'fizz'; }
@@ -38,23 +41,20 @@ var fizzbuzzFunc = function(num) {
   return str;
 };
 
+
 console.log("fizzbuzz success:",
   _.isEqual(_.map(fizzbuzzArray, fizzbuzzFunc), ['fizz', 'buzz', 'fizzbuzz']));
 
 
 /* =====================
-  Writing ajax calls works very much like these higher order functions from
-  underscore. Here's the ajax syntax:
+  Writing ajax calls works very much like these higher order functions from underscore. Here's the ajax syntax:
   $.ajax(<URL GOES HERE>).done(<RECIPE GOES HERE>);
 
-  Your job is to write functions and ajax calls for one of the three provided
-  URLs to pull down and then store data collected through AJAX in variables so
-  that you can work with it. To test your success, prove that you can log the
-  resulting data to your console.
+  Your job is to write functions and ajax calls for one of the three provided URLs to pull down and then store data collected through AJAX in variables so that you can work with it. To test your success, prove that you can log the resulting data to your console.
 
   You may choose whichever dataset you find most interesting.
 
-  NOTE: Because the ajax call happens asynchronously (that is, outside the normal execution order),
+  NOTE: Because the ajax call happens asynchronously (that is,                        outside the normal execution order),
         you'll need to call console.log *within* the function that grabs your data.
         For example:
 
@@ -80,22 +80,54 @@ console.log("fizzbuzz success:",
         It will just log undefined to the console.
 
 ===================== */
+
 var phillySolarInstallationDataUrl = "https://raw.githubusercontent.com/CPLN690-MUSA610/datasets/master/json/philadelphia-solar-installations.json";
 var phillyCrimeDataUrl = "https://raw.githubusercontent.com/CPLN690-MUSA610/datasets/master/json/philadelphia-crime-snippet.json";
 var phillyBikeCrashesDataUrl = "https://raw.githubusercontent.com/CPLN690-MUSA610/datasets/master/json/philadelphia-bike-crashes-snippet.json";
 
 
-/* =====================
-  Data you grab through ajax is just text. You'll need to parse it as javascript
-  objects to really work with it. Use the function JSON.parse on the string you
-  downloaded for the exercise above to parse your strings as javascript objects.
 
-  Remember to call all code within the function body. Use console.log to make sure
-  that this step is completed before moving on!
+
+/* =====================
+  Data you grab through ajax is just text. You'll need to parse it as javascript objects to really work with it. Use the function JSON.parse on the string you downloaded for the exercise above to parse your strings as javascript objects.
+
+  Remember to call all code within the function body. Use console.log to make sure that this step is completed before moving on!
 ===================== */
+
+
+var map = L.map('map', {
+  center: [39.9522, -75.1639],
+  zoom: 14
+});
+var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
+
+
+
+
+var data = $.ajax(phillyBikeCrashesDataUrl).done(function(data){
+  console.log(JSON.parse(data));
+  _.each(JSON.parse(data), function(i){
+    //console.log(i);
+    L.marker([i.LAT, i.LNG]).addTo(map);
+  });
+});
 
 
 /* =====================
   Now that you've properly parsed your data, use _.each to plot the
   dataset you've pulled down.
 ===================== */
+
+
+
+/*
+_.each(data, function(i){
+  L.Marker([i.LAT, i.LNG]).addTo(map);
+});
+*/
